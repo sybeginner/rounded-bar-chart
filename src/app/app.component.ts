be25892ba2ec2a23421f67f90001bc24f083a55e
Lee, Szu Yee <Szuyee.Lee@sc.com>
2020-06-08 12:51:26 +0800

    cleaned up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,19 +15,7 @@ export class AppComponent implements OnInit {
    * init
    */
   ngOnInit(): void {
-    const baseController = Chart.controllers.bar;
-    const originalDraw = Chart.controllers.bar.prototype.draw;
-
-    this.canvas = <any>document.getElementById('chart');
-    const context = this.canvas.getContext('2d');
-    const gradient = this.createGradient(context);
-
     Chart.helpers.drawRoundedTopRectangle = (ctx, x, y, width, height, radius) => {
-      console.log('x: ', x);
-      console.log('y: ',y);
-      console.log('width: ', width);
-      console.log('height: ', height);
-      console.log('radius: ', radius);
       if (height >= 0) {
         ctx.beginPath();
         ctx.moveTo(x + radius, y);
@@ -66,8 +54,6 @@ export class AppComponent implements OnInit {
         const vm = this._view;
         let left, right, top, bottom, signX, signY, borderSkipped;
         let borderWidth = vm.borderWidth;
-
-        console.log('vm: ', vm);
 
         if (!vm.horizontal) {
           // bar
@@ -125,12 +111,6 @@ export class AppComponent implements OnInit {
         top = prevTop + radius;
         const barRadius = top - prevTop;
 
-        // console.log('left: ', left);
-        // console.log('top: ', prevTop);
-        // console.log('right: ', right);
-        // console.log('bottom: ', bottom);
-        //
-        // console.log('bottom - top: ', bottom - top);
         const gdt = ctx.createLinearGradient(left, top, right, bottom);
         if (top < bottom) {
           gdt.addColorStop(0, '#0086fb');
@@ -162,53 +142,9 @@ export class AppComponent implements OnInit {
     // @ts-ignore
     Chart.defaults.global.datasets.roundedBar = Chart.defaults.global.datasets.bar;
     Chart.controllers.roundedBar = Chart.controllers.bar.extend({
-      // draw() {
-      //   // @ts-ignore
-      //   Chart.controllers.bar.prototype.draw.call(this);
-      //   const ctx = this.chart.ctx;
-      //   const chartArea = this.chart.chartArea;
-      //   const width = chartArea.right - chartArea.left;
-      //   const height = chartArea.bottom - chartArea.top;
-      //   Chart.helpers.drawRoundedTopRectangle(ctx, chartArea.left, chartArea.top, width, height, width/2);
-      // }
       // @ts-ignore
       dataElementType: Chart.elements.RoundedTopRectangle
     });
-
-    // console.log('Chart: ', Chart);
-    // console.log('Chart defaults: ', Chart.defaults);
-    // console.log('Chart controllers: ', Chart.controllers);
-    // console.log('Chart controllers.bar: ', Chart.controllers.bar.prototype.draw);
-    // console.log('Chart helpers: ', Chart.helpers);
-    // // @ts-ignore
-    // console.log('Chart elements: ', Chart.elements);
-    // // @ts-ignore
-    // console.log('Chart DatasetController: ', Chart.DatasetController);
-    // Chart.helpers.roundedBar = Chart.helpers.bar;
-
-    // @ts-ignore
-    // Chart.elements.roundedBar = Chart.elements.bar;
-
-    // @ts-ignore
-    // const custom = Chart.DatasetController.extend(Chart.controllers.bar.prototype);
-
-    // const custom = Chart.controllers.bar.extend({
-      // linkScales: () => {
-      //   console.log('link scales');
-      // },
-      // initialize: (chart, datasetIndex)  => {
-      //   console.log('initialize');
-      //   baseController.prototype.initialize.apply(this, arguments);
-      // },
-      // draw: (ease) => {
-      //   console.log('drawing');
-      //   baseController.prototype.draw.apply(this, arguments);
-      // }
-    // });
-
-    // custom.linkScales = Chart.helpers.noop;
-
-    // Chart.controllers.roundedBar = custom;
 
     this.myChart = new Chart('roundedBarChart', {
       type: 'roundedBar',
@@ -216,9 +152,7 @@ export class AppComponent implements OnInit {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', '1', '2', '3', '4', '5', '6'],
         datasets: [{
           label: '# of Votes',
-          data: [3, 2, 1, 3, 6, 3, -3, 6, -1, -6, 5, 8],
-          backgroundColor: gradient,
-          borderColor: gradient,
+          data: [3, 2, 4, 3, 6, 3, -3, 6, -1, -6, 5, 8],
           borderWidth: 0,
           // barThickness: 'flex',
           barPercentage: 0.5,
@@ -253,59 +187,5 @@ export class AppComponent implements OnInit {
         }
       }
     });
-
-    // this.myChart = new Chart('chart', {
-    //   type: 'bar',
-    //   data: {
-    //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    //     datasets: [{
-    //       label: '# of Votes',
-    //       data: [12, 19, 3, -19 , 2, 3],
-    //       backgroundColor: [
-    //         'rgba(255, 99, 132, 0.2)',
-    //         'rgba(54, 162, 235, 0.2)',
-    //         'rgba(255, 206, 86, 0.2)',
-    //         'rgba(75, 192, 192, 0.2)',
-    //         'rgba(153, 102, 255, 0.2)',
-    //         'rgba(255, 159, 64, 0.2)'
-    //       ],
-    //       // backgroundColor: gradient,
-    //       borderColor: [
-    //         'rgba(255, 99, 132, 1)',
-    //         'rgba(54, 162, 235, 1)',
-    //         'rgba(255, 206, 86, 1)',
-    //         'rgba(75, 192, 192, 1)',
-    //         'rgba(153, 102, 255, 1)',
-    //         'rgba(255, 159, 64, 1)'
-    //       ],
-    //       // borderColor: gradient,
-    //       borderWidth: 5,
-    //       // barThickness: 'flex',
-    //       // barPercentage: 0.5,
-    //     },
-    //      ]
-    //   },
-    //   options: {
-    //     scales: {
-    //       yAxes: [{
-    //         ticks: {
-    //           beginAtZero: true
-    //         }
-    //       }]
-    //     },
-    //   }
-    // });
-  }
-
-  /**
-   * create gradient
-   */
-  createGradient(context: CanvasRenderingContext2D): CanvasGradient {
-    const gradient = context.createLinearGradient(0, 0, 0, 800);
-    gradient.addColorStop(0, '#0086fb');
-    gradient.addColorStop(0.5, '#50d3f2');
-    gradient.addColorStop(0.5, '#d1d1d1');
-    gradient.addColorStop(1, '#9e9e9e');
-    return gradient;
   }
 }
